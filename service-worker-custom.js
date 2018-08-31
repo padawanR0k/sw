68a32f6de9a243cdc20502827fbfeb793c760d0c
이유록 <rok@mshuttle.co.kr>
2018-08-31 19:21:55 +0900

    Updates

--- service-worker-custom.js
+++ service-worker-custom.js
@@ -3,7 +3,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 var CACHE_NAME = '캐시-스토리지1';
 // 캐시하고 싶은 리소스
 var urlsToCache = [
-    '/public/media/img.jpg',
+    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
     // '/public/css/font-awesome.min.css'
 ];
 
